82e9dd19ab4ef236460eff116c5162b508a01097
danielambda <daniel.gevorgyan25@gmail.com>
2025-05-07 12:54:29 +0300

    feat: additional instance for GIsUnit

--- src/Telegram/Bot/DSL/Classes/IsUnit.hs
+++ src/Telegram/Bot/DSL/Classes/IsUnit.hs
@@ -1,6 +1,6 @@
 module Telegram.Bot.DSL.Classes.IsUnit (IsUnit(..)) where
 
-import GHC.Generics (Generic, Rep, U1(U1), to)
+import GHC.Generics (Generic, Rep, U1(U1), to, D1, C1, M1 (..))
 
 class IsUnit u where
   unitValue :: u
@@ -16,4 +16,7 @@ class GIsUnit u where
 
 instance GIsUnit U1 where
   gunitValue = U1
+
+instance GIsUnit a => GIsUnit (D1 m0 (C1 m1 a)) where
+  gunitValue = M1 (M1 gunitValue)
 
